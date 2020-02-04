4ca9bea85cc02a03f31646e58535f96e5307444c
hxq <hxq@live.com>
2020-02-04 19:50:51 +0800

    more thread

--- src/test/java/me/ihxq/projects/pna/benchmark/BenchmarkRunner.java
+++ src/test/java/me/ihxq/projects/pna/benchmark/BenchmarkRunner.java
@@ -1,19 +1,20 @@
 package me.ihxq.projects.pna.benchmark;
 
 import me.ihxq.projects.pna.PhoneNumberLookup;
+import me.ihxq.projects.pna.algorithm.AnotherBinarySearchAlgorithmImpl;
 import me.ihxq.projects.pna.algorithm.BinarySearchAlgorithmImpl;
-import me.ihxq.projects.pna.algorithm.NewBinarySearchAlgorithmImpl;
-import me.ihxq.projects.pna.algorithm.SimpleLookupAlgorithmImpl;
+import me.ihxq.projects.pna.algorithm.SequenceLookupAlgorithmImpl;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
@@ -37,51 +38,50 @@ public class BenchmarkRunner {
                 .timeout(TimeValue.minutes(1))
                 .mode(Mode.AverageTime)
                 .timeUnit(TimeUnit.NANOSECONDS)
+                .threads(3)
                 .build();
 
         new Runner(opt).run();
     }
 
-    private static List<String> phoneNumbers = Stream.generate(() -> {
-        long phoneNumber = (long) (ThreadLocalRandom.current().nextDouble(1D, 2D) * 1000_000_000_0L);
-        return String.valueOf(phoneNumber);
-    }).limit(200_000)
-            .collect(Collectors.toList());
+    @State(Scope.Thread)
+    public static class PhoneNumbers {
+        private final List<String> phoneNumbers;
+        private int index = 0;
 
-    static int index = 0;
-
-    private static String getPhoneNumber() {
-        if (index == phoneNumbers.size()) {
-            index = 0;
+        public PhoneNumbers() {
+            this.phoneNumbers = Stream.generate(() -> {
+                long phoneNumber = (long) (ThreadLocalRandom.current().nextDouble(1D, 2D) * 1000_000_000_0L);
+                return String.valueOf(phoneNumber);
+            }).limit(2_000_000)
+                    .collect(Collectors.toCollection(ArrayList::new));
         }
-        return phoneNumbers.get(index++);
-    }
 
-    private static PhoneNumberLookup phoneNumberLookup1;
-    private static PhoneNumberLookup phoneNumberLookup2;
-    private static PhoneNumberLookup phoneNumberLookup3;
-    static {
-        try {
-            phoneNumberLookup1 = new PhoneNumberLookup(new BinarySearchAlgorithmImpl());
-            phoneNumberLookup2 = new PhoneNumberLookup(new NewBinarySearchAlgorithmImpl());
-            phoneNumberLookup3 = new PhoneNumberLookup(new SimpleLookupAlgorithmImpl());
-        } catch (Exception e) {
-            e.printStackTrace();
+        public String getPhoneNumber() {
+            if (index == phoneNumbers.size()) {
+                index = 0;
+            }
+            return phoneNumbers.get(index++);
         }
     }
 
+
+    private static PhoneNumberLookup binarySearchLookup = new PhoneNumberLookup(new BinarySearchAlgorithmImpl());
+    private static PhoneNumberLookup anotherBinarySearchLookup = new PhoneNumberLookup(new AnotherBinarySearchAlgorithmImpl());
+    private static PhoneNumberLookup sequenceLookup = new PhoneNumberLookup(new SequenceLookupAlgorithmImpl());
+
     @Benchmark
-    public void lookup1() throws IOException, URISyntaxException {
-        phoneNumberLookup1.lookup(getPhoneNumber());
+    public void binarySearchLookup(PhoneNumbers phoneNumbers) {
+        binarySearchLookup.lookup(phoneNumbers.getPhoneNumber());
     }
 
     @Benchmark
-    public void lookup2() throws IOException, URISyntaxException {
-        phoneNumberLookup2.lookup(getPhoneNumber());
+    public void anotherBinarySearchLookup(PhoneNumbers phoneNumbers) {
+        anotherBinarySearchLookup.lookup(phoneNumbers.getPhoneNumber());
     }
 
     @Benchmark
-    public void lookup3() throws IOException, URISyntaxException {
-        phoneNumberLookup3.lookup(getPhoneNumber());
+    public void sequenceLookup(PhoneNumbers phoneNumbers) {
+        sequenceLookup.lookup(phoneNumbers.getPhoneNumber());
     }
 }
